9634ac37ad824aea644c929c9d8439ef79d653d6
MorpKnight <christoffelsihombing@gmail.com>
2023-09-11 21:57:49 +0700

    init commit

---
+++ TK3_Kelompok9/KalkulatorDuaBahasa.java
@@ -0,0 +1,46 @@
+package TK3_Kelompok9;
+
+
+/**
+ * Write a description of class KalkulatorDuaBahasa here.
+ *
+ * @author TK2_Kelompok9
+ * @version (a version number or a date)
+ */
+public class KalkulatorDuaBahasa {
+    public KalkulatorDuaBahasa(){
+        
+    }
+
+    public int tambah(int a, int b){
+        return a + b;
+    }
+
+    public int kurang(int a, int b){
+        return a - b;
+    }
+
+    public int kali(int a, int b){
+        return a * b;
+    }
+
+    public int bagi(int a, int b){
+        return a / b;
+    }
+
+    public int add(int a, int b){
+        return a + b;
+    }
+
+    public int substract(int a, int b){
+        return a - b;
+    }
+
+    public int multiply(int a, int b){
+        return a * b;
+    }
+
+    public int divide(int a, int b){
+        return a / b;
+    }
+}
